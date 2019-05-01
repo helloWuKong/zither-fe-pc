595bd0377a2de0111ce0e948a095a0add8a1f859
wangpan <wangpan@laiye.com>
2019-05-01 20:24:35 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+const Home = () => (
+  <div>
+    <Helmet title="首页" />
+    <h1>Hello Next</h1>
+  </div>
+)
+
+export default Home
